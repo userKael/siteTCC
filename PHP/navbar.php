9e317f962ee65ce332344d924fef9786b4dd457a
userKael <alveskael00@gmail.com>
2021-08-11 19:02:56 -0300

    ajustando arquivos

---
+++ PHP/navbar.php
@@ -0,0 +1,22 @@
+
+<body>
+    <header>
+        <nav class=" shadow-sm p-3 mb-5 bg-white rounded navbar fixed-top navbar-light" style="background-color:white">
+            <div class="container-fluid">
+                <img class="logo" src="../IMGs/logo3.png" alt="logo">
+                <form class="d-flex">
+                    <ul class="nav-links nav nav-tabs">
+                        <li class="li-links nav-item"><a  class="nav-link " href="../index.html">HOME</a></li>
+                        <li class="li-links nav-item"><a class="nav-link " href="./dicas_exerc.php">DICAS DE
+                                EXERCÍCIOS</a></li>
+                        <li class="li-links nav-item"><a class="nav-link" href="./cada_biotipo.php">CADA
+                                BIOTIPO</a>
+                        </li>
+                    </ul>
+                    <a class="cta" href="./sobrenos.php"> <input type="button" class="button-nav" value="SOBRE NÓS" style="color: #06386B;"></a>
+                </form>
+            </div>
+        </nav>
+    </header>
+
+</html>
--- PHP/sobrenos.php
+++ PHP/sobrenos.php
@@ -25,25 +25,10 @@
 </head>
 
 <body>
-    <header>
-        <nav class=" shadow-sm p-3 mb-5 bg-white rounded navbar fixed-top navbar-light" style="background-color:white">
-            <div class="container-fluid">
-                <img class="logo" src="../IMGs/logo3.png" alt="logo">
-                <form class="d-flex">
-                    <ul class="nav-links nav nav-tabs">
-                        <li class="li-links nav-item"><a class="nav-link" href="../index.html">HOME</a></li>
-                        <li class="li-links nav-item"><a class="nav-link " href="../HTML/dicas_exerc.html">DICAS DE
-                                EXERCÍCIOS</a></li>
-                        <li class="li-links nav-item"><a class="nav-link" href="../HTML/cada_biotipo.html">CADA
-                                BIOTIPO</a>
-                        </li>
-                    </ul>
-                    <a class="cta" href="../HTML/sobrenos.html"> <input type="button" class="button-nav"
-                            value="SOBRE NÓS" style="color: #06386B;"></a>
-                </form>
-            </div>
-        </nav>
-    </header>
+<?php 
+include './navbar.php'
+?>
+
 
 
 
