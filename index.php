c3f89e30cd1aad88e2de73906f7a788d658723c2
userKael <alveskael00@gmail.com>
2021-08-11 13:36:18 -0300

    criando arquivo cabeçalho.php

--- index.php
+++ index.php
@@ -21,32 +21,14 @@
 
 
 
-    <title>GUDER Sua SAUDE</title>
-</head>
+    <title>GUDER SAÚDE</title>
+    </head>
 
 <body>
-    <header>
-        <nav class=" shadow-sm p-3 mb-5 bg-white rounded navbar fixed-top navbar-light" style="background-color:white">
-            <div class="container-fluid">
-                <img class="logo" src="./IMGs/logo3.png" alt="logo">
-                <form class="d-flex">
-                    <ul class="nav-links nav nav-tabs">
-                        <li class="li-links nav-item"><a style="color: #38b319c7;" class="nav-link active"
-                                href="./index.html">HOME</a></li>
-                        <li class="li-links nav-item"><a class="nav-link " href="./HTML/dicas_exerc.html">DICAS DE
-                                EXERCÍCIOS</a></li>
-                        <li class="li-links nav-item"><a class="nav-link" href="./HTML/cada_biotipo.html">CADA
-                                BIOTIPO</a>
-                        </li>
-                    </ul>
-                    <a class="cta" href="./HTML/sobrenos.html"> <input type="button" class="button-nav"
-                            value="SOBRE NÓS" style="color: #06386B;"></a>
-                </form>
-            </div>
-        </nav>
-    </header>
 
-
+<?php 
+include 'Cabeçalho.php'
+?>
 
     <div id="div_intro">
         <h1>VOCÊ REALMENTE SE CONHECE?</h1>
