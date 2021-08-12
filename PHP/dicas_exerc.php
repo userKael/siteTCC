73661279f565464e5971dc6a94d30ef20f455d59
userKael <alveskael00@gmail.com>
2021-08-12 20:42:56 -0300

    alterando formatações

--- PHP/dicas_exerc.php
+++ PHP/dicas_exerc.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" type="text/css" href="./CSS/style1.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="../CSS/style2.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
--- PHP/navbar.php
+++ PHP/navbar.php
@@ -1,3 +1,6 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="./CSS/style2.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
 <body>
     <header>
@@ -6,11 +9,9 @@
                 <img class="logo" src="../IMGs/logo3.png" alt="logo">
                 <form class="d-flex">
                     <ul class="nav-links nav nav-tabs">
-                        <li class="li-links nav-item"><a  class="nav-link " href="../index.html">HOME</a></li>
-                        <li class="li-links nav-item"><a class="nav-link " href="./dicas_exerc.php">DICAS DE
-                                EXERCÍCIOS</a></li>
-                        <li class="li-links nav-item"><a class="nav-link" href="./cada_biotipo.php">CADA
-                                BIOTIPO</a>
+                        <li class="li-links nav-item"><a class="nav-link " href="../index.html">HOME</a></li>
+                        <li class="li-links nav-item"><a class="nav-link " href="./dicas_exerc.php">DICAS DEEXERCÍCIOS</a></li>
+                        <li class="li-links nav-item"><a class="nav-link" href="./cada_biotipo.php">CADABIOTIPO</a>
                         </li>
                     </ul>
                     <a class="cta" href="./sobrenos.php"> <input type="button" class="button-nav" value="SOBRE NÓS" style="color: #06386B;"></a>
@@ -19,4 +20,4 @@
         </nav>
     </header>
 
-</html>
+    </html>
