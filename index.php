e420db60b4898ba8a8fd3953a16c6d3abfa15eea
userKael <alveskael00@gmail.com>
2021-09-02 01:03:12 -0300

    separando a navabar, e alterando arquivos de html para php

--- index.php
+++ index.php
@@ -5,83 +5,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="./JavaScript/main.js"></script>
-    <script src="./JavaScript/login.js"></script>
-    <link rel="stylesheet" href="./CSS/style.css">
-    <link rel="stylesheet" href="./CSS/responsive.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-    <title>GUDER SAUDE - TESTE</title>
-</head>
+<?php  include './PHP/head.php';?>
 
 <body id="corpo">
-    <header role="navigation" class="fixed" id="cabecalho">
-        <nav class="navbar navbar-light bg-light navbar-expand shadow" id="navbar">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                    <img src="./IMGs/logo.png" alt="logo" id="logo">
-                </a>
-                <div id="ul-nav">
-                    <ul class="navbar-nav me-4 mb-1 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="./index.html" id="color-a">TESTE</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#" id="color-b">CADA BIOTIPO</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#" id="color-c">DICAS SOBRE EXERCÍCIOS</a>
-                        </li>
-                        <li class="nav-item">
-                            <div class="dropdown">
-                                <button type="submit" class="btn btn-outline-success rounded-circle " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                                    </svg></button>
-                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                    <li><a class="dropdown-item" href="./perfil_do_usuario.html">PERFIL</a></li>
-                                    <li><a class="dropdown-item" href="./login.html">ENTRAR</a></li>
-                                    <!-- <li><a class="dropdown-item" href="#">SAIR</a></li> -->
-                                </ul>
-                            </div>
 
-
-                        </li>
-
-                    </ul>
-                </div>
-                <li class="nav-item dropdown" id="drop-down-responsive">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Menu
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="./index.html">TESTE</a></li>
-                        <li><a class="dropdown-item" href="#">CADA BIOTIPO</a></li>
-                        <li><a class="dropdown-item" href="#">DICAS SOBRE EXERCÍCIO</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li>
-                            <a href="./perfil_do_usuario.html" class="nav-link">
-                                <button type="submit" class="btn btn-outline-success rounded-circle" id="bt-perfil"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                                    </svg></button>PERFIL
-
-                            </a>
-
-                        </li>
-                    </ul>
-                </li>
-            </div>
-        </nav>
-    </header>
-
+<?php  include './PHP/cabecalho.php';?>
+<title>GUDER SAUDE - TESTE</title>
+ 
 
     <div id="titulo">
         <h1 class="text-center">Você Realmente se Conhece?</h1>
@@ -161,7 +91,7 @@
         <nav class="navbar bottom navbar-light bg-light">
             <div class="container-fluid">
                 <p style="padding-top: 10px;">© 2021 Guder Saúde - Todos os direitos reservados</p>
-                <a class="nav-link" href="./documentacao.html">Documentação</a>
+                <a class="nav-link" href="./documentacao.php">Documentação</a>
             </div>
         </nav>
     </footer>
---
+++ perfil_do_usuario.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<?php include './PHP/head.php'?>
+<title>GUDER SAUDE - PERFIL</title>
+
+<body id="corpo">
+    
+<?php include './PHP/cabecalho.php';?>
+
+    <div id="div_historico">
+
+        <div class="shadow p-3 mb-5 bg-light rounded border border-1 border-primary" id="historico_usuario">
+            <h5>Histórico</h5>
+            <hr>
+        </div>
+
+    </div>
+
+
+    <div id="div_dados">
+        <div class="shadow p-3 mb-5 bg-light rounded border border-1 border-success" id="dados_usuario">
+
+            <img src="https://www.lance.com.br/files/article_main/uploads/2021/02/28/603bdef934423.jpeg" class="rounded-circle" alt="..." width="70px" height="70px">
+
+            <p>Nome</p>
+            <hr>
+            <h5>Meus dados</h5>
+
+
+        </div>
+
+    </div>
+</body>
+
+</html>
