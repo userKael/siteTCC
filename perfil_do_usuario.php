640529ef9ee1f4d68ad7626f4deefef0ab17ecf9
userKael <alveskael00@gmail.com>
2021-09-04 19:22:42 -0300

    estilo do historico introduzido

--- perfil_do_usuario.php
+++ perfil_do_usuario.php
@@ -29,6 +29,45 @@ if (!isset($_SESSION['email'])) {
             <div class="shadow p-3 mb-5 bg-light rounded border border-1 border-primary" id="historico_usuario">
                 <h5>Histórico</h5>
                 <hr>
+                <div class="accordion accordion-flush" id="accordionFlushExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingOne">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                1º Teste
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body alert-primary">
+                                Aqui ficara os Resultados e dados do 1º teste
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingTwo">
+                            <button class="accordion-button collapsed " type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                2º Teste
+                            </button>
+                        </h2>
+                        <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body alert-primary">
+                                Aqui ficara os Resultados e dados do 2º teste
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingThree">
+                            <button class="accordion-button collapsed " type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                                3º Teste
+                            </button>
+                        </h2>
+                        <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body alert-primary">
+                                Aqui ficara os Resultados e dados do 3º teste
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
 
         </div>
