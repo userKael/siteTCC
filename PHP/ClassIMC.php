cfbe60fa9d6614baf498095d7cc09db0a28a623a
userKael <alveskael00@gmail.com>
2021-09-05 12:13:50 -0300

    comecando a fazer as classificações do imc

--- PHP/ClassIMC.php
+++ PHP/ClassIMC.php
@@ -1,9 +1,104 @@
 <?php
 class cla_imc
 {
-    public function imc6($imc){
+    public function imc1($imc,$sexo){
 
-        
+        $retorno="";
+
+        if($sexo == 'f'){
+            if($imc < 15.3){
+
+                $retorno = "Está abaixo do peso ideal";
+
+            }
+            elseif($imc >=15.4 && $imc <=23){
+
+                $retorno = "Está no peso ideal";
+
+            }elseif($retorno >= 23.1 && $imc <= 27.7){
+
+                $retorno = "Está com sobrepeso";
+
+            }elseif($imc>27.8){
+
+                $retorno = "Está no estágio da obesidade";
+            }
+        }
+        elseif($sexo == "m")
+        {
+            if($imc < 14.8){
+
+                $retorno = "Está abaixo do peso ideal";
+
+            }
+            elseif($imc >=14.9 && $imc <=21.9){
+
+                $retorno = "Está no peso ideal";
+
+            }elseif($retorno >= 22 && $imc <= 25.9){
+
+                $retorno = "Está com sobrepeso";
+
+            }elseif($imc>25.9){
+
+                $retorno = "Está no estágio da obesidade";
+            }
+
+
+        }
+
+        return $retorno;
+
+    }
+
+    public function im2($imc,$sexo){
+
+        $retorno="";
+
+        if($sexo == 'f'){
+            if($imc < 19.3){
+
+                $retorno = "Está abaixo do peso ideal";
+
+            }
+            elseif($imc >=19.4 && $imc <=24.2){
+
+                $retorno = "Está no peso ideal";
+
+            }elseif($retorno >= 24.3 && $imc <= 28.7){
+
+                $retorno = "Está com sobrepeso";
+
+            }elseif($imc>28.8){
+
+                $retorno = "Está no estágio da obesidade";
+            }
+        }
+        elseif($sexo == "m")
+        {
+            if($imc < 19.2){
+
+                $retorno = "Está abaixo do peso ideal";
+
+            }
+            elseif($imc >=19.3 && $imc <=23.7){
+
+                $retorno = "Está no peso ideal";
+
+            }elseif($retorno >= 23.8 && $imc <= 27.7){
+
+                $retorno = "Está com sobrepeso";
+
+            }elseif($imc>27.8){
+
+                $retorno = "Está no estágio da obesidade";
+            }
+
+        }
+
+        return $retorno;
+
+
 
     }
 
--- PHP/imc.php
+++ PHP/imc.php
@@ -23,15 +23,43 @@ if (
     $rotina_exer = addslashes(filter_input(INPUT_POST, 'rotina_exercicios'));
     $idade = $cad->idade($data_nascimento);
 
-    if ($cad->bd_teste($data_nascimento, $altura, $peso, $sexo, $email) == true) {
+    if ($peso < 299)
+    {
+        if ($idade < 12) 
+        {
+            echo "<script>alert('Desculpe, A idade minima é de 12 Anos :(');";
+            echo "location.href='../teste_imc.php'</script>";
 
-        $result = $peso / ($altura * $altura);
+        } 
+        elseif ($cad->bd_teste($data_nascimento, $altura, $peso, $sexo, $email) == true) 
+        {
+            $result = $peso / ($altura * $altura);
 
-        
-    } else {
-        echo "<script>alert('Ops deu algo de errado :( ');";
+            if($idade>=12 && $idade<=13)
+            {
+               echo $imc->imc1($result,$sexo);
+
+            }elseif($idade>=14 && $idade<=15){
+
+                echo $imc->im2($result,$sexo);
+                   
+            }
+
+        } 
+        else 
+        {
+            echo "<script>alert('Ops deu algo de errado :( ');";
+            echo "location.href='../teste_imc.php'</script>";
+        }
     }
-} else {
+    else 
+    {
+        echo "<script>alert('O peso deve ser menor que 300 :( ');";
+        echo "location.href='../teste_imc.php'</script>";
+    }
+} 
+else 
+{
     echo "<script>alert('Digite todos os campos');";
     echo "location.href='../teste_imc.php'</script>";
 }
