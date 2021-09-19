d8d80bf2070eab69040313646262c8b0c1219731
userKael <alveskael00@gmail.com>
2021-09-19 17:44:44 -0300

    todos cadastros já a mostra

--- SistWeb/index.php
+++ SistWeb/index.php
@@ -13,24 +13,32 @@
 </head>
 
 <body id="corpo">
-    <?php include './PHP/cabecalho.php' ?>
+    <header role="navigation" class="fixed" id="cabecalho">
+        <nav class="navbar navbar-light bg-light navbar-expand shadow" id="navbar">
+            <div class="container" style="justify-content: center;">
+                <a class="navbar-brand" href="#">
+                    <img src="./IMGs/logo.png" alt="logo" id="logo">
+                </a>
+            </div>
+        </nav>
+    </header>
 
     <div class="container" id="escolha">
 
         <div class="row align-items-center">
             <div class="col">
                 <div class="card" style="width: 18rem;">
-                   
+
                     <div class="card-body">
                         <h5 class="card-title">Ver cadastros</h5>
                         <p class="card-text">-Ver clientes cadastrados <br> -Excluir cadastros <br> -Adicionar cadastros <br> -Ver resultados de testes</p>
-                        <a href="#" class="btn btn-primary">VER</a>
+                        <a href="./cadastros.php" class="btn btn-primary">VER</a>
                     </div>
                 </div>
             </div>
             <div class="col">
                 <div class="card" style="width: 18rem;">
-                    
+
                     <div class="card-body">
                         <h5 class="card-title">Aterar textos do resultado</h5>
                         <p class="card-text">-Ver textos de resultados <br> - Alterar textos</p>
@@ -41,7 +49,7 @@
             </div>
             <div class="col">
                 <div class="card" style="width: 18rem;">
-                   
+
                     <div class="card-body">
                         <h5 class="card-title"></h5>
                         <p class="card-text"></p>
