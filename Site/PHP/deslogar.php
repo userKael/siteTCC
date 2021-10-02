5bab0ca3f479cc30e624bd39e86627537f100532
userKael <alveskael00@gmail.com>
2021-10-02 16:31:10 -0300

    limpando variaveis ao deslogar

--- Site/PHP/deslogar.php
+++ Site/PHP/deslogar.php
@@ -2,5 +2,9 @@
 session_start();
 unset($_SESSION['email']);
 unset($_SESSION['nome']);
+unset($_SESSION['senha']);
+unset($_SESSION['somatotipo']);
+unset($_SESSION['imc']);
+unset($_SESSION['imc_bd']);
 header("location: ../index.php");
 ?>
