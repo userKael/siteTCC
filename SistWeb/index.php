4f152e7447c8a19c6730cbf528d51d56f6fcb055
userKael <alveskael00@gmail.com>
2021-10-03 21:34:18 -0300

    adicionando mais uma opção de alteração

--- SistWeb/index.php
+++ SistWeb/index.php
@@ -28,8 +28,8 @@
             <div class="col">
                 <div class="card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Ver cadastros</h5>
-                        <p class="card-text">-Ver clientes cadastrados <br> -Excluir cadastros <br> -Adicionar cadastros <br> -Ver resultados de testes</p>
+                        <h5 class="card-title">Cadastros</h5>
+                        <p class="card-text">- Ver clientes cadastrados <br> - Excluir cadastros <br> - Adicionar cadastros <br> - Ver resultados de testes</p>
                         <div class="text-center">
                             <a href="./cadastros.php" class="btn btn-primary">VER</a>
                         </div>
@@ -39,8 +39,8 @@
             <div class="col">
                 <div class="card" style="width: 18rem;">
                     <div class="card-body ">
-                        <h5 class="card-title">Aterar textos do resultado</h5>
-                        <p class="card-text">-Ver textos de resultados <br> - Alterar textos <br> - Alterar recomendações</p>
+                        <h5 class="card-title">Textos do resultado</h5>
+                        <p class="card-text">- Ver textos de resultados <br> - Alterar textos <br> - Alterar recomendações</p>
                         <br>
                         <div class="text-center">
                             <a href="./textos.php" class="btn btn-primary">VER</a>
@@ -51,10 +51,10 @@
             <div class="col">
                 <div class="card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title"></h5>
-                        <p class="card-text"></p>
+                        <h5 class="card-title">Conteúdo</h5>
+                        <p class="card-text">-Ver e alterar dicas de exercícios <br> -Ver e alterar Sobre cada somatotipo <br> <br></p>
                         <div class="text-center">
-                            <a href="#" class="btn btn-primary">Sem ideia</a>
+                            <a href="#" class="btn btn-primary">VER</a>
                         </div>
                     </div>
                 </div>
