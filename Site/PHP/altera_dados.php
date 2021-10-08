c6d94caa7a00e5c43f2690bf9d8e389008179e4b
userKael <alveskael00@gmail.com>
2021-10-08 16:20:11 -0300

    adicionando mais um requisito para alteração

--- Site/PHP/altera_dados.php
+++ Site/PHP/altera_dados.php
@@ -5,39 +5,52 @@ include './conexaobd.php';
 if (
     isset($_POST["email"]) && !empty($_POST["email"])
     && isset($_POST["senha"]) && !empty($_POST["senha"])
-    && isset($_POST["nome"])  && !empty($_POST["nome"])
+    && isset($_POST["nome"]) && !empty($_POST["nome"])
 ) {
 
-    $emailn = addslashes(filter_input(INPUT_POST, 'email',FILTER_SANITIZE_EMAIL));
+    $emailn = addslashes(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL));
     $senha = addslashes(filter_input(INPUT_POST, 'senha'));
-    $nome = addslashes(filter_input(INPUT_POST, 'nome',FILTER_SANITIZE_STRING));
-
+    $nome = addslashes(filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING));
     $email = $_SESSION['email'];
 
-    try {
+    $comando = $con->prepare("SELECT * FROM cadastro WHERE email= ?");
+    $comando->bindParam(1, $email);
+    $comando->execute();
 
-        $comando4 = $con->prepare("UPDATE cadastro SET email =?, senha =?, nome = ? WHERE email =?");
-        $comando4->bindParam(1, $emailn);
-        $comando4->bindParam(2, $senha);
-        $comando4->bindParam(3, $nome);
-        $comando4->bindParam(4, $email);
-        $comando4->execute();
+    if ($comando->rowCount() == 0) {
+        try {
 
-        if ($comando4->rowCount() > 0) {
+            $comando4 = $con->prepare("UPDATE cadastro SET email =?, senha =?, nome = ? WHERE email =?");
+            $comando4->bindParam(1, $emailn);
+            $comando4->bindParam(2, $senha);
+            $comando4->bindParam(3, $nome);
+            $comando4->bindParam(4, $email);
+            $comando4->execute();
 
-            $_SESSION['email'] = $emailn;
-            $_SESSION['nome'] = $nome;
-            $_SESSION['senha'] = $senha;
-            $retorno = "Cadastro alterado com Sucesso!";
+            if ($comando4->rowCount() > 0) {
 
-        }else{
-            $retorno = "Erro ao alterar cadastro !";
+                $_SESSION['email'] = $emailn;
+                $_SESSION['nome'] = $nome;
+                $_SESSION['senha'] = $senha;
+                $retorno = "Cadastro alterado com Sucesso!";
+            } 
+            else 
+            {
+                $retorno = "Erro ao alterar cadastro !";
+            }
+        } 
+        catch (PDOException $erro) 
+        {
+            $retorno = "Erro " . $erro->getMessage();
         }
-
-    } catch (PDOException $erro) {
-        $retorno = "Erro " . $erro->getMessage();
+    } 
+    else 
+    {
+        $retorno = "Email Já Existente, Tente Outro.";
     }
-} else {
+} 
+else 
+{
     $retorno = "Digite todos os campos";
 }
 echo $retorno;
