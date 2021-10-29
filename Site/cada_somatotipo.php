b33e0e8e5b355946ff9a1a2ebfe37e05c1b91e46
eduardo souza <eduardo.dsoz33@gmail.com>
2021-10-29 19:46:59 -0300

    c

--- Site/cada_somatotipo.php
+++ Site/cada_somatotipo.php
@@ -27,6 +27,36 @@
                     <img src="./IMGs/ectomorfo.jpg" class="border border-success border-3" alt="" width="190px">
                 </div>
             </div>
+            <div class="col" id="col1">
+                <div class="shadow p-3 mb-5 bg-light rounded border border-primary border border-3" id="teste1">
+                    <h5>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, quisquam non aspernatur officiis tempore repellat harum voluptatum ipsam labore? Perspiciatis doloribus sapiente repudiandae, ut quas porro molestias saepe asperiores est! Lorem ipsum
+                        dolor sit amet consectetur adipisicing elit. Aliquam, soluta sed? Ut sint sunt accusantium explicabo libero. Iusto optio ad necessitatibus perspiciatis debitis, praesentium consequuntur dolore sit maiores molestiae at. Lorem ipsum
+
+                    </h5>
+
+                </div>
+            </div>
+            <div class="col">
+                <div class="" id="teste2">
+                    <img src="./IMGs/endomorfo.jpg" class="border border-success border-3" alt="" width="190px">
+                </div>
+            </div>
+            <div class="col" id="col1">
+                <div class="shadow p-3 mb-5 bg-light rounded border border-primary border border-3" id="teste1">
+                    <h5>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, quisquam non aspernatur officiis tempore repellat harum voluptatum ipsam labore? Perspiciatis doloribus sapiente repudiandae, ut quas porro molestias saepe asperiores est! Lorem ipsum
+                        dolor sit amet consectetur adipisicing elit. Aliquam, soluta sed? Ut sint sunt accusantium explicabo libero. Iusto optio ad necessitatibus perspiciatis debitis, praesentium consequuntur dolore sit maiores molestiae at. Lorem ipsum
+
+                    </h5>
+
+                </div>
+            </div>
+            <div class="col">
+                <div class="" id="teste2">
+                    <img src="./IMGs/mesomorfo.jpg" class="border border-success border-3" alt="" width="190px">
+                </div>
+            </div>
 
         </div>
     </div>
