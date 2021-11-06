c46bbe0678e7425d484e6578f877bd94f0cfc074
userKael <alveskael00@gmail.com>
2021-11-06 17:35:27 -0300

    adicionando alterações na formatação das imgs

--- Site/cada_somatotipo.php
+++ Site/cada_somatotipo.php
@@ -2,6 +2,7 @@
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <?php include './PHP/head.php' ?>
     <link rel="stylesheet" href="./CSS/cada_biotipo.css">
@@ -10,21 +11,45 @@
 
 <body id="corpo">
     <?php include './PHP/cabecalho.php' ?>
-    <div class="container" style="margin: 0;">
+    <div class="titulo-somatotipo mt-4">
+        <h1 style="color:#06396b;">
+            Teste de IMC
+        </h1>
+        <hr>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col " id="div-txt-imc">
+               
+                <h5>
+                    O Índice de Massa Corporal (IMC), refere-se ao teste feito para identificar por meio de uma fórmula matemática se você está com o peso condizente coma sua altura, e também para certificar que você não está abaixo do peso, com sobrepeso ou até mesmo com índice de obesidade.
+                    <br>
+                    Por meio do teste a seguir você saberá como está a situação de sua saúde segundo a matemática, mas... a seguir faremos mais testes
+
+                </h5>
+
+                <hr>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5">
         <div class="row">
             <div class="col" id="col1">
                 <div class="shadow p-3 mb-5 bg-light rounded border border-primary border border-3" id="teste1">
-                    <h5>
+                    <p>
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, quisquam non aspernatur officiis tempore repellat harum voluptatum ipsam labore? Perspiciatis doloribus sapiente repudiandae, ut quas porro molestias saepe asperiores est! Lorem ipsum
                         dolor sit amet consectetur adipisicing elit. Aliquam, soluta sed? Ut sint sunt accusantium explicabo libero. Iusto optio ad necessitatibus perspiciatis debitis, praesentium consequuntur dolore sit maiores molestiae at. Lorem ipsum
-
-                    </h5>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem neque aspernatur aliquid atque eius quae fugit debitis sit libero veniam, a obcaecati vero incidunt voluptatem quasi possimus, sint dolores recusandae.
+                    </p>
 
                 </div>
             </div>
             <div class="col">
                 <div class="" id="teste2">
-                    <img src="./IMGs/ectomorfo.jpg" class="border border-success border-3" alt="" width="190px">
+                    <img src="./IMGs/ectomorfo.jpg" class="border border-success border-3" alt="" width="170px" height="120%">
                 </div>
             </div>
             <div class="col" id="col1">
@@ -39,7 +64,8 @@
             </div>
             <div class="col">
                 <div class="" id="teste2">
-                    <img src="./IMGs/endomorfo.jpg" class="border border-success border-3" alt="" width="190px">
+                    <img src="./IMGs/mesomorfo.jpg" class="border border-success border-3" alt="" width="170px" height="120%">
+
                 </div>
             </div>
             <div class="col" id="col1">
@@ -54,12 +80,13 @@
             </div>
             <div class="col">
                 <div class="" id="teste2">
-                    <img src="./IMGs/mesomorfo.jpg" class="border border-success border-3" alt="" width="190px">
+                    <img src="./IMGs/endomorfo.jpg" class="border border-success border-3" alt="" width="170px" height="120%">
+
                 </div>
             </div>
-
         </div>
     </div>
-   
+
 </body>
+
 </html>
