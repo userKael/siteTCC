29d64a00826636925a954300acbfe7d7edf126d9
Nykollaslight <nykollaslira@gmail.com>
2021-11-06 21:01:34 -0300

    Criação e desenvolvimento da tela de dicas de exercicios

--- Site/dicas_exercicios.php
+++ Site/dicas_exercicios.php
@@ -3,19 +3,141 @@
 
 <head>
     <?php include './PHP/head.php' ?>
+
+    <link rel="stylesheet" href="./CSS/dicas.css">
     <title>Document</title>
 </head>
 
-<body id="">
+<body id="corpo-dicas">
     <?php include './PHP/cabecalho.php' ?>
     
-    <div class="container">
+    <div id="txt-exp-dicas">
+    <p>
+    Vamos entender um pouco sobre alguns exercícios e suas vantagens para o nosso corpo?
+    </p>
+    <p>
+    Primeiramente sabemos que a base da nossa saúde é manter a rotina de exercícios ativa, nosso corpo depende disso em algum mento.
+    </p>
+    <p>
+    Para a gente ter uma ideia de o quão é bom atividades físicas, além de prevenir doenças físicas como níveis de colesterol, diabetes hipertensão etc. Como também pode prevenir de doenças psicológicas igual a depressão e ansiedade.
+    </p>
+    </p>
+    Você pode conferir uma delas aqui!
+    </p>
+
+    </div>
+
+    <div class="container" id="dicas-container">
       <div class="row row-cols-2">
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-        <div class="col">Column</div>
+        
+       <div class="col-dicas">
+          <div class="div-cima" style="text-align: center;">
+            imagem
+          </div>
+        </div>
+        
+        <div class="col-dicas">
+          <div class="div-cima" style="text-align: center;">
+            imagem
+          </div>
+        </div>
+        
+        <div class="col-dicas">
+          <div class="div-baixo" style="text-align: center;">
+            Texto sobre o exercício
+          </div>
+        </div>
+        
+        <div class="col-dicas">
+          <div class="div-baixo" style="text-align: center;">
+            Texto sobre o exercício
+          </div>
+        </div>
+        
       </div>
+
+      <div class="row row-cols-2 continue">
+        
+        <div class="col-dicas">
+           <div class="div-cima" style="text-align: center;">
+            imagem
+           </div>
+         </div>
+         
+         <div class="col-dicas">
+           <div class="div-cima" style="text-align: center;">
+            imagem
+           </div>
+         </div>
+         
+         <div class="col-dicas">
+           <div class="div-baixo" style="text-align: center;">
+            Texto sobre o exercício
+           </div>
+         </div>
+         
+         <div class="col-dicas">
+          <div class="div-baixo" style="text-align: center;">
+            Texto sobre o exercício
+           </div>
+         </div>
+         
+       </div>
+
+        <div class="row row-cols-2 continue">
+        
+          <div class="col-dicas">
+            <div class="div-cima" style="text-align: center;">
+              imagem
+            </div>
+          </div>
+          
+          <div class="col-dicas">
+            <div class="div-cima" style="text-align: center;">
+              imagem
+            </div>
+          </div>
+          
+          <div class="col-dicas">
+            <div class="div-baixo" style="text-align: center;">
+              Texto sobre o exercício
+            </div>
+          </div>
+          
+          <div class="col-dicas">
+            <div class="div-baixo" style="text-align: center;">
+              Texto sobre o exercício
+            </div>
+          </div>
+        </div>
+
+        <div class="row row-cols-2 continue">
+        
+        <div class="col-dicas">
+          <div class="div-cima" style="text-align: center;">
+            imagem
+          </div>
+        </div>
+        
+        <div class="col-dicas">
+          <div class="div-cima" style="text-align: center;">
+            imagem
+          </div>
+        </div>
+        
+        <div class="col-dicas">
+          <div class="div-baixo" style="text-align: center;">
+            Texto sobre o exercício
+          </div>
+        </div>
+        
+        <div class="col-dicas">
+          <div class="div-baixo" style="text-align: center;">
+            Texto sobre o exercício
+          </div>
+        </div>
+      </div>
+
   </div>
 </body>
 </html>
