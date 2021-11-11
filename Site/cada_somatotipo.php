3d8532a6caed33ea2b541dca019744699f0e333c
eduardo souza <eduardo.dsoz33@gmail.com>
2021-11-11 17:01:42 -0300

    alterando textos

--- Site/cada_somatotipo.php
+++ Site/cada_somatotipo.php
@@ -13,7 +13,7 @@
     <?php include './PHP/cabecalho.php' ?>
     <div class="titulo-somatotipo mt-4">
         <h1 style="color:#06396b;">
-            Teste de IMC
+            O que são os somatótipos?
         </h1>
         <hr>
     </div>
@@ -24,9 +24,9 @@
             <div class="col " id="div-txt-imc">
                
                 <h5>
-                    O Índice de Massa Corporal (IMC), refere-se ao teste feito para identificar por meio de uma fórmula matemática se você está com o peso condizente coma sua altura, e também para certificar que você não está abaixo do peso, com sobrepeso ou até mesmo com índice de obesidade.
+                   Diferente de biotipo que está relacionado a genética da pessoa e acaba sendo imutável, os somatótipos são o estado atual de cada corpo e que pode ser mudado com treinamento e dieta correta.
                     <br>
-                    Por meio do teste a seguir você saberá como está a situação de sua saúde segundo a matemática, mas... a seguir faremos mais testes
+                  O termo somatótipo criado por Sheldon, em 1940, se refere a classificação física do corpo humano, que pode ser aplicada tanto para homens quanto para mulheres, sendo divido em 3 tipos: Ectomorfo, Mesomorfo e Endomorfo e é através deles que podemos dizer como o nosso corpo está atualmente.
 
                 </h5>
 
@@ -40,8 +40,7 @@
             <div class="col" id="col1">
                 <div class="shadow p-3 mb-5 bg-light rounded border border-primary border border-3" id="teste1">
                     <h5>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, quisquam non aspernatur officiis tempore repellat harum voluptatum ipsam labore? Perspiciatis doloribus sapiente repudiandae, ut quas porro molestias saepe asperiores est! Lorem ipsum
-                        dolor sit amet consectetur adipisicing elit. Aliquam, soluta sed? Ut sint sunt accusantium explicabo libero. Iusto optio ad necessitatibus perspiciatis debitis, praesentium consequuntur dolore sit maiores molestiae at. Lorem ipsum
+                        Pessoas com o somatótipo ectomorfo geralmente apresentam um corpo mais magro, com ombros estreitos e membros compridos, além do seu metabolismo rápido que faz com que eles tenham a capacidade de perder peso muito facilmente e ter dificuldade em ganhar.
                    </h5>
 
                 </div>
@@ -54,9 +53,7 @@
             <div class="col" id="col1">
                 <div class="shadow p-3 mb-5 bg-light rounded border border-primary border border-3" id="teste1">
                     <h5>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, quisquam non aspernatur officiis tempore repellat harum voluptatum ipsam labore? Perspiciatis doloribus sapiente repudiandae, ut quas porro molestias saepe asperiores est! Lorem ipsum
-                        dolor sit amet consectetur adipisicing elit. Aliquam, soluta sed? Ut sint sunt accusantium explicabo libero. Iusto optio ad necessitatibus perspiciatis debitis, praesentium consequuntur dolore sit maiores molestiae at. Lorem ipsum
-
+                      Corpos mesomorfos são caracterizados por um padrão de corpo onde a pessoa naturalmente é magra porém com mais músculos, além de membros superiores mais desenvolvidos. Pessoas com o corpo mesomorfo também tem facilidade em ganho de massa muscular.  
                     </h5>
 
                 </div>
@@ -70,8 +67,7 @@
             <div class="col" id="col1">
                 <div class="shadow p-3 mb-5 bg-light rounded border border-primary border border-3" id="teste1">
                     <h5>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, quisquam non aspernatur officiis tempore repellat harum voluptatum ipsam labore? Perspiciatis doloribus sapiente repudiandae, ut quas porro molestias saepe asperiores est! Lorem ipsum
-                        dolor sit amet consectetur adipisicing elit. Aliquam, soluta sed? Ut sint sunt accusantium explicabo libero. Iusto optio ad necessitatibus perspiciatis debitis, praesentium consequuntur dolore sit maiores molestiae at. Lorem ipsum
+                       O somatótipo endomorfo é caracterizado por um corpo mais largo e arredondado, com maior quantidade de gordura acumulada e metabolismo lento, além disso pessoas com o corpo endomorfo tem uma grande dificuldade em perder peso.
 
                     </h5>
 
